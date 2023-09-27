40df78e7a55440767f3a0e6ae0ce431d27eed1a8
starstealer <rizkasmt@gmail.com>
2023-09-27 20:07:27 +0800

    first commit

---
+++ posttest-2/script.js
@@ -0,0 +1,11 @@
+let anchorSelector = 'a[href^="#"]';
+        let anchorList = document.querySelectorAll(anchorSelector);
+        anchorList.forEach(link => {
+            link.onclick = function(e) {
+                e.preventDefault();
+                let destination = document.querySelector(this.hash);
+                destination.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
